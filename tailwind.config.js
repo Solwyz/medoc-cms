32b2c36bb5f14f8a1d12b2b94589f4e77c9f6904
Nizamudeen N <nizamudeen@solwyz.com>
2025-04-03 10:59:46 +0530

    login

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
- 
+
   theme: {
     extend: {
       fontFamily: {
         AnekLatin: ["Anek Latin", "sans-serif"],
-        Popins: ["Poppins", "sans-serif"],
+        Poppins: ["Poppins", "sans-serif"], // Fixed the spelling
       },
       backgroundImage: {
- 
- 
+        adminLogin: "url('/src/Cms/Assets/bg-admin.png')", // Fixed path issue
       },
       colors: {
-        "gray-textColor": "#AEAEAE",
+        grayTextColor: "#AEAEAE", // Fixed naming convention
       },
     },
   },
